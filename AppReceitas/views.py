f7d972156f2bf0d9a036da8206bb1a19460f1eda
edithcarollaine <edith.carollaine12@gmail.com>
2024-02-01 18:56:47 -0300

    primeiro commit do projeto de criar um site de receitas com Django

---
+++ AppReceitas/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'recipe/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]})
+
+def recipe(request, id):
+    return render(request, 'recipe/pages/recipe-view.html', context={'recipe': make_recipe()})
