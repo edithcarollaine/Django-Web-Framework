aa13f51db5fe439e8172a14b1f6a14307e456da8
edithcarollaine <edith.carollaine12@gmail.com>
2024-02-02 15:46:54 -0300

    alterado urls

--- AppReceitas/urls.py
+++ AppReceitas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'receitas'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipe/<int:id>/', views.recipe, name='receita'),
