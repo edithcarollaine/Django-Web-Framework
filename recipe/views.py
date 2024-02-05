0fd0067b69d515daca884fbd7b60b5817562a467
edithcarollaine <edith.carollaine12@gmail.com>
2024-02-05 15:08:09 -0300

    melhorado a opção de encontrar recipe e caso não encontrar a recipe, trazer error 404

--- recipe/views.py
+++ recipe/views.py
@@ -10,12 +10,10 @@ def home(request):
     return render(request, 'recipe/pages/home.html', context={'recipes': recipes})
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('id')
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, 
+                                                    is_published=True)
+                                                    .order_by('-id'))
     
-    if not recipes:
-        raise Http404('Not found, sorry. Keep trying!')
-    
-
     return render(request, 'recipe/pages/category.html',
                   context={'recipes': recipes, 
                             'title': f'{recipes[0].category.name} - Category |'})
